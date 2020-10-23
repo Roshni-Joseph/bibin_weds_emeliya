7a7f77fbb1a66a19ab20dbef132f913448a5f17c
sebin <sebinjosepht@gmail.com>
2020-10-23 23:35:16 +0530

    updated the content

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,24 +4,27 @@ import './Navbar.css'
 
 class Navbar extends Component {
     
-    state = { clicked:false, setNavbar:false }
+    state = { clicked:false , backgroundColor: 'rgba(134,206,219 ,.4)'}
+      
     handleClick = () =>{
         this.setState({clicked: !this.state.clicked})
     }
-    changeBackground =() =>{
-        // console.log(window.scrollY)
-        if(window.scrollY >= 300){
-            this.setState({setNavbar:true})
-            console.log(window.scrollY)
-        }else{
-            this.setState({setNavbar:false})
+    listenScrollEvent = e => {
+        if (window.scrollY > 400) {
+          this.setState({backgroundColor: 'rgba(134,206,219 ,.9)'})
+        } else {
+          this.setState({backgroundColor: 'rgba(134,206,219 ,.4)'})
         }
-    }
+      }
+    
+      componentDidMount() {
+        window.addEventListener('scroll', this.listenScrollEvent)
+      }
     
     render() { 
         return (  
-            <nav className=" navbar-items {setNavbar ? 'active' : 'navbar-items'}">
-                {/* <h1 className="navbarlogo"> BIBIN & EMELIYA</h1> */}
+            <nav className=" navbar-items" style={{backgroundColor: this.state.backgroundColor}}>
+                <h1 className="navbarlogo"> Bibin & Emeliya <span>ARE GETTING MARRIED</span></h1>
                 <div className="menu-icon" onClick={this.handleClick} >
                     <i className={this.state.clicked ? 'close': 'hamburger'}></i>
                 </div>
--- src/components/Pages/Ourstory.js
+++ src/components/Pages/Ourstory.js
@@ -21,9 +21,9 @@ class Ourstory extends Component{
                                 <span className="timeline-icon">
                                     <img src="/images/toys.svg" alt="Born and grown"/>
                                 </span>
-                                <h3>Bron and Grown</h3>
+                                <h3>Born and Grown</h3>
                                 <h5>1990'<sup>s</sup></h5>
-                                <p>Born and grown at two ends of Kerala, Malappuram and Kasargod. Bibin completed schooling at Holy Cross School and Emeliya from Navodaya. When one has good childhood memories, they grow up to be happy individuals .! and that's true...</p>
+                                <p>Bibin and Emeliya...both born and grown into beautiful hilly villages of Kerala .. Vettilappara and Bheemanadi . Both have experienced a pleasant childhood and a marvellous school life . Emeliya was lucky enough to be a Navodaya   girl . Bibin spent his schooling at Holy Cross School.</p>
                             </div>
                             
                         </div>
@@ -33,7 +33,7 @@ class Ourstory extends Component{
                             <div className="w100p">
                                 <h3>Schools and College</h3>
                                 <h5>2000'<sup>s</sup></h5>
-                                <p>This is the Bron and grown story of bibin and emeliya asdfkh asdhf asdfhsadjkfhkasdjf asdfhkasdf asdfhkajsdfhdsf khasdf asdfkhasdf asdfdsafjkhasdfjh ahsdfkjhasdfjsdfhsd</p>
+                                <p>Youth is the jubilant and vibrant period in life , lot of energy to explore new horizon. Bibin completed his graduation from St.Joseph's college Devagiri. Emeliya persued BHMS from Fr.Muller's College Manglore.</p>
                             </div>
                             
                         </div>
@@ -58,8 +58,8 @@ class Ourstory extends Component{
                                     <img src="/images/success.svg" alt="Life stone"/>
                                 </span>
                                 <h3>Life stone</h3>
-                                <h5>15-10-2019</h5>
-                                <p>This is the Bron and grown story of bibin and emeliya asdfkh asdhf asdfhsadjkfhkasdjf asdfhkasdf asdfhkajsdfhdsf khasdf asdfkhasdf asdfdsafjkhasdfjh ahsdfkjhasdfjsdfhsd</p>
+                                <h5>2018 '<sup>s</sup></h5>
+                                <p>Fulfilling passion is the highest  achievement. Now Bibin is teacher and Emeliya is a doctor .</p>
                             </div>
                             
                         </div>
@@ -68,8 +68,8 @@ class Ourstory extends Component{
                         <div className="flex-center story-content">
                             <div className="w100p">
                                 <h3>Firs Meeting</h3>
-                                <h5>15-05-2020</h5>
-                                <p>This is the Bron and grown story of bibin and emeliya asdfkh asdhf asdfhsadjkfhkasdjf asdfhkasdf asdfhkajsdfhdsf khasdf asdfkhasdf asdfdsafjkhasdfjh ahsdfkjhasdfjsdfhsd</p>
+                                <h5>27-06-2020</h5>
+                                <p>Destiny and desire  paved way to the much anticipated day..!  Their FIRST meeting</p>
                             </div>
                         </div>
                         <div className="story-img">
@@ -94,7 +94,7 @@ class Ourstory extends Component{
                                 </span>
                                 <h3>The Wedding Day</h3>
                                 <h5>24-10-2020</h5>
-                                <p>This is the Bron and grown story of bibin and emeliya asdfkh asdhf asdfhsadjkfhkasdjf asdfhkasdf asdfhkajsdfhdsf khasdf asdfkhasdf asdfdsafjkhasdfjh ahsdfkjhasdfjsdfhsd</p>
+                                <p>“Every heart sings a song, incomplete, until another heart whispers back..!" And here they are moving forward to complete the sonnet of life❣️</p>
                             </div>
                             
                         </div>
