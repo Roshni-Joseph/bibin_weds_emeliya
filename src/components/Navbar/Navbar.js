a778d7ee1586a44c695ccf923c1b003d34448352
sebin <sebinjosepht@gmail.com>
2020-10-24 10:25:05 +0530

    updated the description

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import {MenuItems} from "./MenuItems"
 import './Navbar.css'
 
@@ -12,8 +12,10 @@ class Navbar extends Component {
     listenScrollEvent = e => {
         if (window.scrollY > 400) {
           this.setState({backgroundColor: 'rgba(134,206,219 ,.9)'})
+          this.setState({fc: '#fff'})
         } else {
           this.setState({backgroundColor: 'rgba(134,206,219 ,.4)'})
+          this.setState({fc: '#229db8'})
         }
       }
     
@@ -24,7 +26,7 @@ class Navbar extends Component {
     render() { 
         return (  
             <nav className=" navbar-items" style={{backgroundColor: this.state.backgroundColor}}>
-                <h1 className="navbarlogo"> Bibin & Emeliya <span>ARE GETTING MARRIED</span></h1>
+                <h1 className="navbarlogo" style={{color: this.state.fc}}> Bibin & Emeliya <span style={{color: this.state.fc}}>ARE GETTING MARRIED</span></h1>
                 <div className="menu-icon" onClick={this.handleClick} >
                     <i className={this.state.clicked ? 'close': 'hamburger'}></i>
                 </div>
@@ -32,7 +34,7 @@ class Navbar extends Component {
                     {MenuItems.map((item, index)=>{
                         return(
                             <li key="{index}">
-                                <a className={item.cName} href={item.url}>
+                                <a className={item.cName} href={item.url} onClick={this.handleClick} style={{color: this.state.fc}}>
                                     {item.titel}
                                 </a>
                             </li>
